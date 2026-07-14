57a4b2da22eaf7963c3e12329c32ed69bf3e7ce5
Owen Wu <owenwdd01@gmail.com>
2026-07-14 14:11:52 -0400

    Added Title Link and better styling to individual objects

--- components/DatabasePage/IndividualObject.tsx
+++ components/DatabasePage/IndividualObject.tsx
@@ -1,4 +1,5 @@
 import BackButton from "@/utils/BackButton";
+import StarWeaveTitle from "@/utils/Title";
 
 type IndividualObjectProps = {
   name: string;
@@ -6,12 +7,18 @@ type IndividualObjectProps = {
   fromQuery: string;
 };
 
+const stellarObjectCategory: Record<string, string> = {
+  horizons: "Horizon Major Body",
+  exoplanet: "Exoplanet",
+};
+
 export default function IndividualObject({ name, location, fromQuery }: IndividualObjectProps) {
   return (
-    <div className="h-screen bg-nebulaBG">
+    <div className="flex flex-col items-center min-h-screen bg-nebulaBG">
       <BackButton destination={`/database?query=${fromQuery}`} />
-      {name}
-      {location}
+      <StarWeaveTitle size={5} />
+      <p className="general-text">{name}</p>
+      <p className="general-text">{stellarObjectCategory[location]}</p>
     </div>
   );
 }
