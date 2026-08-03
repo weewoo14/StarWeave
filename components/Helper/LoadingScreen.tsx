0ce450765121289a9110845df5d97cb2ce5952a1
Owen Wu <owenwdd01@gmail.com>
2026-08-03 18:25:17 -0400

    Added new loading screen message

--- components/Helper/LoadingScreen.tsx
+++ components/Helper/LoadingScreen.tsx
@@ -4,7 +4,7 @@ export default function LoadingScreen() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-nebulaBG">
       <h1 className="font-syne text-white text-[10vw] md:text-[5vw] z-10">
-        Loading data...
+        Loading data. Please wait for a short moment.
       </h1>
       <LoaderCircle color='#ffffff' className='size-[4vw] animate-spin'/>
     </div>
